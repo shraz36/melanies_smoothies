857e6bbd72fde6f25e556db01bbd63127c91784a
shraz36 <71567674+shraz36@users.noreply.github.com>
2024-04-04 17:11:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=mydataframe.to_pandas()
@@ -36,8 +36,8 @@ if ingredients_list:
 	for fruit_chosen in ingredients_list:
 		ingredients_string += fruit_chosen + ' '
 		
-		#search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-		#st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+		search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+		st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 		
 		st.subheader(fruit_chosen + 'Nutrition Information')
 		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
