d3fb46abcc7535c1e2d32bd457a312562446745d
shraz36 <71567674+shraz36@users.noreply.github.com>
2024-04-04 16:03:43 +0100

    Update streamlit_app.py
    added python requests and fruityvice to the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,8 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
+	
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
@@ -46,5 +48,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	
         st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
+
+#New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
