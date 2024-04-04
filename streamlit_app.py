5e92578d6e72838d0aaec290cf44e654764d6fe1
shraz36 <71567674+shraz36@users.noreply.github.com>
2024-04-04 16:33:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,18 +28,18 @@ if ingredients_list:
 	ingredients_string = ''
 
 	for fruit_chosen in ingredients_list:
-        	ingredients_string += fruit_chosen + ' '
-        	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+		ingredients_string += fruit_chosen + ' '
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+	my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        	values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     #st.write(my_insert_stmt)
     #st.stop()
-    time_to_insert = st.button('Submit Order')
+	time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+	if time_to_insert:
+		session.sql(my_insert_stmt).collect()
 	
-        st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
+		st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
