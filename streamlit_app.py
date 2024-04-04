d0acb80e22f49689265a13aed6b4938e9c784e58
shraz36 <71567674+shraz36@users.noreply.github.com>
2024-04-04 16:21:18 +0100

    Update streamlit_app.py
    unsure changes to make the code work

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -42,8 +43,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	
         st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
